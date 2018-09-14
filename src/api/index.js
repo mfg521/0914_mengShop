9a773dd61ebe5e6ddc63aed7b7f98fff4702dd48
tom_glb <good_glb@mfg.com>
2018-09-14 09:53:06 +0400

    second commit

--- src/api/index.js
+++ src/api/index.js
@@ -2,12 +2,14 @@
 包含n个接口请求函数的模块
  */
   import  ajax from './ajax'
+  //const BASE_URL='http://localhost:4000'
+  const  BASE_URL='/api'
 
   //1、根据经纬度获取位置详情]
-  export  const reqAddress =(geohsh)=>ajax(`/position/${geohsh}`)
+  export  const reqAddress =(geohsh)=>ajax(`${BASE_URL}/position/${geohsh}`)
 
   //2、获取食品分类列表
-  export  const reqFootTypes=()=>ajax('index_category')
+  export  const reqFootCategorys=()=>ajax(BASE_URL+'/index_category')
 
   //3、根据经纬度获取商铺列表
-  export const reqShops=(longitude,latitude)=>ajax('/shops',{longitude,latitude})
+  export const reqShops=(longitude,latitude)=>ajax(BASE_URL+'/shops',{longitude,latitude})
